cfa55846a55fda08c403586e1ef3264f41bd5d88
adinugrhoo <adi.smarispat@gmail.com>
2018-10-10 21:40:07 +0700

    updated contetn header

---
+++ js/typewriting.js
@@ -0,0 +1,15 @@
+var instance = new TypeIt('#element', {
+    strings: ['This is my string!'],
+    //-- Other options...
+});
+
+new TypeIt('#welcome', {
+    strings: 'This is a simple string.',
+    speed: 50,
+    autoStart: false
+  });
+
+// The simplest example
+new TypeIt('.type-it', {
+    strings: 'This is my string!'
+});
